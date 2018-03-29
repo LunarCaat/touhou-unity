e4f6c01fcb8be074cebc35dbe2821c38187541e0
setseroZZ <resetos35@gmail.com>
2018-03-29 10:59:43 -0500

    Added movement to player

--- GensokyoUnityChan/Assets/Player.cs
+++ GensokyoUnityChan/Assets/Player.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Range(0.0f, 10.0f)] // create a slider in the editor and set limits on moveSpeed
+    public float moveSpeed = 3f;
+
+    private Rigidbody _rigidbody;
+
+    float _vx;
+    float _vy;
+
+    // Use this for initialization
+    void Awake() {
+        _rigidbody = GetComponent<Rigidbody>();
+        if (_rigidbody == null) // if Rigidbody is missing
+            Debug.LogError("Rigidbody component missing from this gameobject");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        _vx = Input.GetAxisRaw("Horizontal");
+        _vy = Input.GetAxisRaw("Vertical");
+        
+
+        _rigidbody.velocity = new Vector3(_vx * moveSpeed, _vy * moveSpeed, 0);
+    }
 }
